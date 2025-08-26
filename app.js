5984ecbf23f69aad10184751e2276482bdd007f3
RAODSYSTEM007 <ceo@raodsystem.com>
2025-08-25 20:10:58 -0400

    Proyecto base Node: app.js + axios + .gitignore

---
+++ app.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+async function main() {
+  console.log("Hola Bootcamp 👋");
+  try {
+    const res = await axios.get("https://api.github.com");
+    console.log("Estado:", res.status);
+    console.log("Servidor:", res.headers.server);
+  } catch (err) {
+    console.error("Falla al consultar API:", err.message);
+  }
+}
+main();
